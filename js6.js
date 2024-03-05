a434382764b90b3190c6fc8357a394f5d731ebe6
MaheshFSD <maheshfsdeveloper@gmail.com>
2024-03-05 23:09:28 +0530

    JS Code 14

--- js6.js
+++ js6.js
@@ -19,3 +19,20 @@ function func3 () {
 const funcReturned = func3();
 console.log(funcReturned, ' ========= this is the function that is returned from the called function .....');
 funcReturned(); // this gives the output after calling the returned function -------- i.e ------ i am inside a function that is returned.....
+
+// these are called Higher order functions-----  a function which take a function as input or return a function are called higher order functions .....
+
+
+// Important Array Functions 
+// ForEach
+const arr = [5,4,3,2,1];
+function add2ToAll (num, index) { 
+    console.log(num, index ,' -------------');
+    console.log(`${num} + 2 = ${num + 2}, '''''' adding 2to all the elememnts ------`)
+}
+// for (let ind = 0; ind<arr.length; ind++) {
+//     console.log(ind, arr[ind], ' =========== for look ind and value')
+//     add2ToAll(arr[ind], ind);
+// }
+arr.forEach(add2ToAll); //
+arr.forEach(num=>console.log(`${num+2} = after adding 2 to all elements --===`));
