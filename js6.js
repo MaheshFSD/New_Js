06b09c30b5f229300b5435afab5ca212ce3eff9c
MaheshFSD <maheshfsdeveloper@gmail.com>
2024-03-05 22:24:48 +0530

    JS Code 14

--- js6.js
+++ js6.js
@@ -8,3 +8,14 @@ console.log(param, ' ======== parameter ===');
 param(); // === from func1 ======= here we are calling param as function cause we passed a function as a callback so we can call it in the func2 function
 }
 func2(func1);
+
+// Function returning a function
+function func3 () {
+    function func4 () {
+        console.log('i am inside a function that is returned.....');
+    }
+    return func4;
+}
+const funcReturned = func3();
+console.log(funcReturned, ' ========= this is the function that is returned from the called function .....');
+funcReturned(); // this gives the output after calling the returned function -------- i.e ------ i am inside a function that is returned.....
