0d7952e5d9a5524a018c52aa13f3ae27ca768d25
MaheshFSD <maheshfsdeveloper@gmail.com>
2024-03-10 23:46:34 +0530

    JS COde 26

--- js13.js
+++ js13.js
@@ -8,9 +8,19 @@ const person = {
     details: function () {
         // console.log(' ------------ person name is', fName, ' and age is', age, ' ============'); // fName is not defined - Error - reference error ---
         console.log(' ------------ person name is', this.fName, ' and age is', person.age, ' ============');
+        console.log(this, ' ================= printing this ================ '); // this is the fill object that called the details function ====
     }
 }
 person.about() // ----- Function inside an object. i.e Method ------ 
 console.log(person.fName, person.age, person.about(), ' ======== printing all the propertis and calling the method of the obj');
 person.details();
+function greet () {
+    let msg = 'Welcome';
+    console.log(' --------- Dear', this.fName, this.age+' '+ ' you are most', msg );
+}
+person.greetfunc = greet;
+person.greetfunc();
+console.log(this, ' ----------- just priniting this'); // window
+console.log(window, ' ----------- just priniting this'); // window
+console.log(this.alert('Hi'),alert('Hello!'),window.alert('Namaste!'), ' ----------- just priniting this');
 
