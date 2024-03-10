98a39665141d820f8d61a6119eefede69c1e2d94
MaheshFSD <maheshfsdeveloper@gmail.com>
2024-03-10 22:49:15 +0530

    JS Code 26

--- js13.js
+++ js13.js
@@ -4,8 +4,13 @@ const person = {
     age: 26,
     about: function(){
         console.log('----- Function inside an object. i.e Method ------ ');
+    },
+    details: function () {
+        // console.log(' ------------ person name is', fName, ' and age is', age, ' ============'); // fName is not defined - Error - reference error ---
+        console.log(' ------------ person name is', this.fName, ' and age is', person.age, ' ============');
     }
 }
 person.about() // ----- Function inside an object. i.e Method ------ 
 console.log(person.fName, person.age, person.about(), ' ======== printing all the propertis and calling the method of the obj');
+person.details();
 
