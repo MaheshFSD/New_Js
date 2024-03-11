42b9d59b4cb47718a584b02e094805c9201076ec
MaheshFSD <maheshfsdeveloper@gmail.com>
2024-03-12 03:10:26 +0530

    JS Code 33

---
+++ js15.js
@@ -0,0 +1,98 @@
+// oop start
+// the requirement is to create no of users objects
+
+const user1 = {
+    fName: 'abhishek',
+    age: 26,
+    graduate: true,
+    gender: 'male',
+    salary: 2000000,
+    details () {
+        console.log(this.fName, + ' ' + this.age, ' - is working hard -');
+    },
+    interests () {
+        console.log(this.fName, ' --- is interested in Coding ... ');
+    }
+};
+
+console.log(user1, ' ============ printing user 1 ---- ');
+
+const user2 = {
+    fName: 'Mahesh',
+    age: 28,
+    graduate: true,
+    gender: 'male',
+    salary: 5000000,
+    details () {
+        console.log(this.fName, + ' ' + this.age, ' - is working smart -');
+    },
+    interests () {
+        console.log(this.fName, ' --- is interested in Coding ... and teaching ---  ');
+    }
+};
+
+console.log(user2, ' ============ printing user 1 ---- ');
+
+// like this writing code for million users is not adviced so,
+
+// solution 1
+function createUser (fName,age,graduate,gender,salary) {
+    const user = {
+        fName,
+        age,
+        graduate,
+        gender,
+        salary,
+        details () {
+            console.log(this.fName, + ' ' + this.age, ' - is working hard -');
+        },
+        interests () {
+            console.log(this.fName, ' --- is interested in Coding ... ');
+        }
+    };
+    return user;
+};
+const userNew1 = createUser('rachit', 30, true, 'male', 250000);
+const userNew2 = createUser('ishan', 33, true, 'male', 300000);
+const userNew3 = createUser('vidya', 31, true, 'female', 200000);
+console.log(userNew1, ' ========== object created from function ------ ');
+console.log(userNew2, ' ========== object created from function ------ ');
+console.log(userNew3, ' ========== object created from function ------ ');
+
+// the solution might look good but see how many times you create users that many times the methods are getting created... so it's not adviced to repeat 
+
+// solution 2 ---- take out the methods and store in another place and reference them
+const userMethods = {
+    details () {
+        console.log(this.fName, + ' ' + this.age, ' - is working hard -');
+    },
+    interests () {
+        console.log(this.fName, ' --- is interested in Coding ... ');
+    }
+}
+
+function createUser1 (fName,age,graduate,gender,salary) {
+    const user = {
+        fName,
+        age,
+        graduate,
+        gender,
+        salary,
+        details:  userMethods.details,
+        interests: userMethods.interests 
+    };
+    return user;
+};
+const userNew4 = createUser1('mohit', 28, true, 'male', 100000);
+const userNew5 = createUser1('siva', 29, true, 'male', 150000);
+const userNew6 = createUser1('nazeer', 30, true, 'male', 200000);
+
+console.log(userNew4, ' ========== object created from function ------ ');
+console.log(userNew5, ' ========== object created from function ------ ');
+console.log(userNew6, ' ========== object created from function ------ ');
+
+// here this solution works good. but what if you have so many methods to add, ypou have to add them to both, objectmethods object and also to the function then only ypou can able to make them work.
+// so there is a high chance that we forget to add them in any of the place, then it causes issues. So
+// thinking about a solution here - Even if we miss adding the method to the object we don't want to cause any issue.
+// technically even if don't write the method again in the function object, it should work.
+
