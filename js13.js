199d35a38693126416182de333a3b72a09f68f15
MaheshFSD <maheshfsdeveloper@gmail.com>
2024-03-11 12:56:20 +0530

    JS Code 27

--- js13.js
+++ js13.js
@@ -22,7 +22,7 @@ person.greetfunc = greet;
 person.greetfunc();
 console.log(this, ' ----------- just priniting this'); // window
 console.log(window, ' ----------- just priniting this'); // window
-console.log(this.alert('Hi'),alert('Hello!'),window.alert('Namaste!'), ' ----------- just priniting this');
+// console.log(this.alert('Hi'),alert('Hello!'),window.alert('Namaste!'), ' ----------- just priniting this');
 
 function newFunc () {
     console.log(this, '------- this inside a function'); // window
@@ -39,3 +39,35 @@ function newFunc2 () {
 newFunc2(); // undefined
 this.newFunc2(); // window
 window.newFunc2(); 
+
+// Call, apply and bind methods
+
+const obj1 = {
+    fName: 'Mahesh',
+    age: 26,
+    details: function () {
+        console.log(this.fName, this.age, ' ========= is a valued member ------');
+    }
+}
+const obj2 = {
+    fName: 'lokesh',
+    age: 28
+}
+// if i want to use the details method of the obj1 with the info of obj2, i can make use of it using call, apply and bind
+// now as details is inside obj1 we have to call it from the obj only
+obj1.details.call(obj2) // now we are calling the obj1 detaiols meyhod by passing obj2 ------- lokesh 28  ========= is a valued member ------
+// lets create a global function and use call on it.
+function funcEx () {
+    console.log(' ------- inside funcEx functiion ==========');
+}
+funcEx(); // ditect call
+window.funcEx();
+this.funcEx();
+// now we can call this function in another way also using call
+funcEx.call(); 
+funcEx.apply();
+funcEx.bind(); // this won't call the function instead returns the function that we can call later
+const funcExNew = funcEx.bind();
+console.log('\n',funcExNew, ' =========== resultant function after binding it ------ ');
+funcExNew();
+
