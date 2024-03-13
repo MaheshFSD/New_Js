fa22f62ab56ed6b69fcbb1be2f55e40d4851a589
MaheshFSD <maheshfsdeveloper@gmail.com>
2024-03-13 18:58:19 +0530

    JS Code 39

--- js20.js
+++ js20.js
@@ -33,3 +33,57 @@ class PetrolBike extends Vehicle {
 const hondaPetrolBike = new PetrolBike ('2 wheeler', 'Red', 200, '2 people');
 console.log(hondaPetrolBike, ' ========= honda instance of petrolbike ------');
 console.log(hondaPetrolBike.wheel, hondaPetrolBike.capacity, hondaPetrolBike.pickup(), hondaPetrolBike.power, ' =========== ');
+
+// class getter and setters 
+class Person {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+    details () {
+        return `${this.firstName} is of age ${this.age}`;
+    }
+} 
+
+const user1 = new Person ('Mahesh', 'G S', 28);
+console.log(user1, ' ========== user1 ----');
+console.log(user1.firstName,user1.lastName, ' ========== user1 properties ----');
+console.log(user1.details(), ' ========== user1 method calling ----');
+
+// here i want to access the details method without paranthesis - but it's not a property, still i want to get the response 
+// we can do this by using getter 
+class Person1 {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+    get details () {
+        return `${this.firstName} is of age ${this.age}`;
+    }
+    nameChange (firstName,lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+} 
+console.log(Person1, ' -------- person1 class --- ');
+const person1User1 = new Person1('Abhishek', 'shukla', 26);
+console.log(person1User1, ' ----- person1 object 0------ ');
+console.log(person1User1.firstName, ' ----- person1 object properties ------ ');
+console.log(person1User1.details, ' ----- person1 object 0------ '); // remember we are not calling it directly here - still we get output - Abhishek is of age 26  ----- person1 object 0------ 
+// because we used get here ----- now if we call it using () it says error saying it's not a function cause now it treats it like property
+
+// setter Example ------ we need to change the values of the properties of the class object, to do that we can follow bellow solutions 
+
+// solution 1
+person1User1.firstName = 'lokesh';
+person1User1.lastName = 'vaas';
+console.log(person1User1, '========= after changing the values ---- ');
+
+// solution 2 --- create a method in the class and through it change the values ----
+person1User1.nameChange('Rakesh', 'modi');
+console.log(person1User1, ' ------- after changing values through method ----');
+
+// solution 3
+
