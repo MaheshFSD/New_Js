77164915baab1538423a2020a819c9d6a2bd5ef2
MaheshFSD <maheshfsdeveloper@gmail.com>
2024-03-18 12:36:44 +0530

    JS Code 48

--- js23.js
+++ js23.js
@@ -9,7 +9,18 @@ function greet() {
     welcome();
 }
 greet();
-// Above is lexical env Example -
+// Above is lexical env and scope chain Example 
 // Above variable msg is lexically available through out the global scope.... 
 // means msg lexical environment is global- 
 // lexical environment - means the scope where you can access the value is called lexical scope
+
+// ex2
+function details () {
+    let fName = 'Mahesh G';
+    console.log(fName, ' --------- Here fName is available through out the details function ---- ');
+    function info () {
+        console.log(fName, ' ------ we can access fName here also as fname is available lexically through the entire details function.--') // fName lexical environment is complete details function
+    }
+    info();
+}
+details ();
