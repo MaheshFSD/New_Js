25763a51218d19fae425240f521a2bde80046a9c
MaheshFSD <maheshfsdeveloper@gmail.com>
2024-03-19 14:45:32 +0530

    JS Code 53

--- js26.js
+++ js26.js
@@ -20,3 +20,14 @@ console.log(document.getElementsByName('nameButton')[1].value = 'Changed Value o
 // getElementsByTagname
 console.log(document.getElementsByTagName('input'), ' ---------- All elemnts with same tag ------- '); 
 console.log(document.getElementsByTagName('input')[1].value = 'Hello Hello', ' ---------- All elemnts with same tag and changed one elemets value  ------- '); 
+
+// querySelector
+console.log(document.querySelector('.gesbcn1'), ' ------ getting the element with the class Name ---- '); // but it gives only the first occurance of the element
+console.dir(document.querySelector('.gesbcn1'), ' ------ getting the element with the class Name ---- '); // but it gives only the first occurance of the element
+// the above two lines are same now but not before. before it usually the human readable like html tag
+
+// to get all the elemnts with the same classname or ny other property use
+// querySelectorAll
+console.log(document.querySelectorAll('.gesbcn1'), ' ---------- gesbcn1 class elemnts list ------ ');
+console.log(document.querySelector('#gebt1'), ' ---------- id elemnt ---- ');
+console.dir(document.querySelector('#gebt1'), ' ---------- id elemnt ---- ');
