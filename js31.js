2cf50cd198327734f7e65bdbb6d4bdf921a4303a
MaheshFSD <maheshfsdeveloper@gmail.com>
2024-03-21 18:37:41 +0530

    JS Code 66

---
+++ js31.js
@@ -0,0 +1,20 @@
+// call stack event loop webapi and callback Queue
+console.log('script starts....');
+const allBtns = document.querySelectorAll('.eventBtns');
+allBtns.forEach(btn => {
+    btn.addEventListener('click', function (e) {
+        let num =0;
+        for(let i=0; i<1000000000;i++) {
+            num+=i;
+        }
+        console.log(num, e, ' --- values ----');
+    })
+});
+
+let variable= 0;
+for(let i=0;i<100000000;i++) {
+    variable+=i;
+}
+console.log(variable, ' -------- variable ---- ');
+console.log('script ends ....');
+
