c7faf1422ae52ad03145823d4477b6af4b205e87
MaheshFSD <maheshfsdeveloper@gmail.com>
2024-03-28 15:22:35 +0530

    JS Code 86

--- advJavaScript/index.js
+++ advJavaScript/index.js
@@ -1,6 +1,11 @@
 // import {QA_ENV} from './utills/constants.js';
 import {QA_ENV,PROD_ENV as prod} from './utills/constants.js';
+import {Person} from './utills/person.js';
 
 console.log('Welcome');
 console.log(QA_ENV, ' -------- ENV ------- ');
 console.log(prod, ' -------- ENV ------- ');
+
+const person = new Person('Mahesh', 'G S', 26, 'Male');
+console.log(person , ' ------ just printing person');
+person.details();
